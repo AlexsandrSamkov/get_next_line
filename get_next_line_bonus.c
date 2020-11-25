e581bd65d12341a50ccffbae52cdf8a6a9760b8e
weambros <weambros@student.21-school.ru>
2020-11-25 16:25:10 +0300

    OK

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: weambros <weambros@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 08:31:59 by weambros          #+#    #+#             */
-/*   Updated: 2020/11/25 06:21:22 by weambros         ###   ########.fr       */
+/*   Updated: 2020/11/25 06:52:35 by weambros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 static int	ft_free(char **s, int res)
 {
@@ -67,18 +67,18 @@ static int	ft_check_line(char **line, char **save)
 
 int			get_next_line(int fd, char **line)
 {
-	static char	*save;
+	static char	*save[4048];
 	int			len;
 
-	if (!line || BUFFER_SIZE <= 0 || fd < 0)
+	if (!line || BUFFER_SIZE <= 0 || fd < 0 || fd > 4048)
 		return (-1);
-	if ((len = ft_read_line(&save, fd)) == -1)
+	if ((len = ft_read_line(&save[fd], fd)) == -1)
 		return (-1);
-	if (len == 0 && save == 0)
+	if (len == 0 && save[fd] == 0)
 	{
 		if (!(*line = ft_strdup("")))
 			return (-1);
-		return (ft_free(&save, 0));
+		return (ft_free(&save[fd], 0));
 	}
-	return (ft_check_line(&*line, &save));
+	return (ft_check_line(&*line, &save[fd]));
 }
--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: weambros <weambros@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 08:48:46 by weambros          #+#    #+#             */
-/*   Updated: 2020/11/25 06:23:10 by weambros         ###   ########.fr       */
+/*   Updated: 2020/11/25 06:18:58 by weambros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 size_t	ft_strlen(const char *s)
 {
